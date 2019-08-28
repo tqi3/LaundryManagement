8dc5c612402f0def2cab7dea945f3bc58aa4485f
fanfankuankuan <fanfankuankuan@live.com>
2019-08-27 22:15:14 -0600

    Add Login Fragment

---
+++ app/src/main/java/com/recycle/laundrymanagement/activity_login.java
@@ -0,0 +1,31 @@
+package com.recycle.laundrymanagement;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class activity_login extends Fragment {
+
+
+    public activity_login() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activity_login, container, false);
+    }
+
+}
